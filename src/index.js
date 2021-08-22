d9b400e00794db92294ad7d9de0f456bd26095a6
William Sercombe <wsercombe@gmail.com>
2021-08-22 11:30:21 -0700

    Update health bars

--- src/index.js
+++ src/index.js
@@ -5,6 +5,33 @@ import './css/styles.css';
 //import Tamagotchi from './js/tamagotchi.js';
 /* eslint-disable no-console */
 
+function disableColors(level) {
+  for (let i = 100; i > 0; i-=10) {
+    $(`.${level}-${i}`).hide();
+  }
+}
+
+function testColors() {
+  const level = "clean";
+  for (let i = 100; i > 0; i-=10) {
+    setTimeout(function(){ 
+      disableColors(level);
+      alert("hey");
+      $(`.${level}-${i}`).show();
+    }, 3000);
+    console.log(`.${level}-${i}`);
+    // setInterval(function() {
+    //   disableColors(level);
+    //   $(`.${level}-${i}`).show();
+    //   /* eslint-disable no-console */
+    //   console.log(`.${level}-${i}`);
+    //   /* eslint-enable no-console */
+    // }, 1000);
+  }
+}
+
+
+testColors();
 var position = 0;
 
 function decipherPosition(position) {
